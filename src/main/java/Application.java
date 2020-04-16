6634a66a37f036f2b2f95a2a9c9e0d8cd52adde8
up201806330 <45789887+up201806330@users.noreply.github.com>
2020-04-16 04:14:43 +0100

    temp

---
+++ src/main/java/Application.java
@@ -0,0 +1,8 @@
+public class Application {
+    public static void main(String[] args) {
+        // Main menu
+        //      New Game
+        //      Load Game
+        //      Options
+    }
+}
