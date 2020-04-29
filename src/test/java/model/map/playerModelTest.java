a5696bf87451ac5b0d7a94dd8ecc0a7bd3d347e8
Joao Sousa <joaosousa.pt@gmail.com>
2020-04-29 18:59:52 +0100

    Slight Update on the Report and Change in Tests

--- src/test/java/model/map/playerModelTest.java
+++ src/test/java/model/map/playerModelTest.java
@@ -21,27 +21,45 @@ public class playerModelTest {
 
 
     @Test
-    public void updatePosTest() {
+    public void updatePosTest1() {
         PlayerModel pm = new PlayerModel(new Position(4, 5), "P", Mockito.mock(TextColor.class));
 
         pm.updatePosition(MapView.COMMAND.DOWN);
         pm.updatePosition(MapView.COMMAND.DOWN);
         pm.updatePosition(MapView.COMMAND.DOWN);
+
         assertEquals(4, pm.getPosition().getX());
         assertEquals(8, pm.getPosition().getY());
+    }
+
+    @Test
+    public void updatePosTest2() {
+        PlayerModel pm = new PlayerModel(new Position(4, 5), "P", Mockito.mock(TextColor.class));
 
         pm.updatePosition(MapView.COMMAND.UP);
-        pm.updatePosition(MapView.COMMAND.LEFT);
-        pm.updatePosition(MapView.COMMAND.LEFT);
-        assertEquals(2, pm.getPosition().getX());
-        assertEquals(7, pm.getPosition().getY());
+        pm.updatePosition(MapView.COMMAND.UP);
+        assertEquals(4, pm.getPosition().getX());
+        assertEquals(3, pm.getPosition().getY());
+    }
 
-        pm.updatePosition(MapView.COMMAND.DOWN);
+    @Test
+    public void updatePosTest3() {
+        PlayerModel pm = new PlayerModel(new Position(4, 5), "P", Mockito.mock(TextColor.class));
+
+        pm.updatePosition(MapView.COMMAND.LEFT);
+        assertEquals(3, pm.getPosition().getX());
+        assertEquals(5, pm.getPosition().getY());
+    }
+
+    @Test
+    public void updatePosTest4() {
+        PlayerModel pm = new PlayerModel(new Position(4, 5), "P", Mockito.mock(TextColor.class));
+
         pm.updatePosition(MapView.COMMAND.RIGHT);
         pm.updatePosition(MapView.COMMAND.RIGHT);
         pm.updatePosition(MapView.COMMAND.RIGHT);
-        assertEquals(5, pm.getPosition().getX());
-        assertEquals(8, pm.getPosition().getY());
+        assertEquals(7, pm.getPosition().getX());
+        assertEquals(5, pm.getPosition().getY());
     }
 
 
