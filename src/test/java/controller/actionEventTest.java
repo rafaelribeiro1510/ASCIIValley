b51b175151c755be2663fd797871c29667b5463e
Joao Sousa <joaosousa.pt@gmail.com>
2020-05-03 16:16:01 +0100

    Added 1 test and folder with Code Coverage Report

--- src/test/java/controller/actionEventTest.java
+++ src/test/java/controller/actionEventTest.java
@@ -5,7 +5,6 @@ import model.Position;
 import org.junit.Before;
 import org.junit.Test;
 
-
 import static controller.GameController.MAP_HEIGHT;
 import static controller.GameController.MAP_WIDTH;
 import static org.junit.Assert.assertEquals;
@@ -24,7 +23,7 @@ public class actionEventTest {
     @Test
     public void actionRightNewChunk() {
         gc.getPlayer().setPosition(chunk1RightBorder);
-        ActionEvent event = new MoveRight(gc, gc.getMapModel());
+        event = new MoveRight(gc, gc.getMapModel());
         gc.processKey(event);
 
         assertEquals(0, gc.getPlayer().getPosition().getX());
@@ -40,4 +39,16 @@ public class actionEventTest {
         assertEquals(MAP_WIDTH - 1, gc.getPlayer().getPosition().getX());
         assertEquals(10, gc.getPlayer().getPosition().getY());
     }
+
+    @Test
+    public void actionDownWaterCollision() {
+        gc.getPlayer().setPosition(chunk1RightBorder);
+        event = new MoveDown(gc, gc.getMapModel());
+        gc.processKey(event);
+        gc.processKey(event);
+
+        assertEquals(MAP_WIDTH - 1, gc.getPlayer().getPosition().getX());
+        assertEquals(11, gc.getPlayer().getPosition().getY());
+    }
+
 }
