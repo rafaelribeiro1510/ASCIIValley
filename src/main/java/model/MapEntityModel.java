f8d5eb2c4993b0929326ffb0c9f2fb6fecc02847
Joao Sousa <joaosousa.pt@gmail.com>
2020-05-03 19:29:01 +0100

    More Small Report Changes

--- src/main/java/model/MapEntityModel.java
+++ src/main/java/model/MapEntityModel.java
@@ -1,11 +1,11 @@
 package model;
 
 import com.googlecode.lanterna.TextColor;
-import model.EntityModel;
-import model.Position;
 
 import java.util.HashMap;
 import java.util.Map;
+
+
 
 class myPair {
     public TextColor myColor;
@@ -14,14 +14,6 @@ class myPair {
     public myPair(TextColor myColor, boolean myCollision) {
         this.myColor = myColor;
         this.myCollision = myCollision;
-    }
-
-    public TextColor getColor() {
-        return myColor;
-    }
-
-    public boolean getCollision() {
-        return myCollision;
     }
 }
 
@@ -41,28 +33,6 @@ public class MapEntityModel extends EntityModel {
             this.color = map2.get(string).myColor;
             this.collision = map2.get(string).myCollision;
         }
-        /*
-        switch (string){
-            case "^": //Stone
-                this.color = new TextColor.RGB(40,40,40);
-                this.collision = true;
-                ;
-                break;
-            case "~": //Water
-                this.color = new TextColor.RGB(0, 204, 255);
-                this.collision = true;
-                break;
-            case ":": //Dirt
-                this.color = new TextColor.RGB(26, 12, 0);
-                this.collision = false;
-                break;
-            case "y": //Grass patch
-                this.color = new TextColor.RGB(0, 204, 0);
-                this.collision = false;
-            default:
-                break;
-        }
-        */
     }
 
 }
