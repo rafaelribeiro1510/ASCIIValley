2a15a8acb793a6be9c328e15fc349e2167a07c90
up201806330 <45789887+up201806330@users.noreply.github.com>
2020-05-23 04:18:53 +0100

    Fixed tool tests

--- src/test/java/com/g64/model/ToolTest.java
+++ src/test/java/com/g64/model/ToolTest.java
@@ -2,63 +2,75 @@ package com.g64.model;
 
 import com.g64.controller.GameController;
 import com.g64.exceptions.Died;
+import com.g64.exceptions.RemoveFromInventory;
 import com.g64.model.entities.EntityModel;
 import com.g64.model.entities.enemy.Mummy;
 import com.g64.model.entities.plant.TallGrassEntity;
+import com.g64.model.entities.target.Target;
 import com.g64.model.items.tools.Axe;
 import com.g64.model.items.tools.Tool;
+import com.g64.model.terrain.NullTerrain;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import org.mockito.Mockito;
-import static org.mockito.Mockito.verify;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
 
 public class ToolTest {
     GameController controller;
-    ChunkModel chunk;
-    InventoryModel inventory = new InventoryModel();
-    InventoryModel inventorySpy = Mockito.spy(inventory);
+    MapModel map;
+    ChunkModel chunkSpy;
+    InventoryModel inventorySpy;
 
-
-    TallGrassEntity grass = new TallGrassEntity(new Position(0,0));
-    Mummy mummy = new Mummy(new Position(1,1));
-    Mummy mummySpy = Mockito.spy(mummy);
-
+    TallGrassEntity grass;
+    Mummy mummySpy;
 
     @SuppressWarnings("unchecked")
     @Before
     public void initChunk(){
+        grass = new TallGrassEntity(new Position(0,0));
+        Mummy mummy = new Mummy(new Position(1,1));
+        mummySpy = Mockito.spy(mummy);
+
+        InventoryModel inventory = new InventoryModel();
+        inventorySpy = Mockito.spy(inventory);
+
         ArrayList<EntityModel> entities = new ArrayList<>(); entities.add(grass); entities.add(mummySpy);
-        chunk = new ChunkModel(1, Mockito.mock(ArrayList.class), Mockito.mock(ArrayList.class), entities);
+        ChunkModel chunk = new ChunkModel(1, Mockito.mock(ArrayList.class), Mockito.mock(ArrayList.class), entities);
+        chunkSpy = Mockito.spy(chunk);
+        doReturn(new NullTerrain(new Position(0,0))).when(chunkSpy).getTerrainAt(any(Position.class));
+
         controller = Mockito.mock(GameController.class);
-        MapModel map = Mockito.mock(MapModel.class); Mockito.when(map.thisChunk()).thenReturn(chunk);
-        Mockito.when(controller.getMapModel()).thenReturn(map);
-        Mockito.when(controller.getInventoryModel()).thenReturn(inventorySpy);
+        map = Mockito.mock(MapModel.class); when(map.thisChunk()).thenReturn(chunkSpy);
+        when(controller.getMapModel()).thenReturn(map);
+        when(controller.getInventoryModel()).thenReturn(inventorySpy);
     }
 
     @Test
-    public void successfulUsage() throws Died {
+    public void successfulUsage() throws Died, RemoveFromInventory {
         Tool tool = new Axe();
-        tool.use(controller, new Position(1,1));
+        tool.accept(new Target(controller, new Position(1,1)));
         verify(mummySpy).reduceHealth(tool.getHitValue());
     }
 
     @Test
-    public void successfulItemDrop() throws Died {
+    public void successfulItemDrop() throws Died, RemoveFromInventory {
         Tool tool = new Axe();
-        tool.use(controller, new Position(1,1));
+        tool.accept(new Target(controller, new Position(1,1)));
 
-        tool.use(controller, new Position(1,1));
+        tool.accept(new Target(controller, new Position(1,1)));
         verify(mummySpy, Mockito.times(2)).reduceHealth(tool.getHitValue());
         verify(inventorySpy).add(mummySpy.getRandomDrop());
     }
 
     @Test
-    public void failedUsage() {
+    public void failedUsage() throws RemoveFromInventory {
         Tool tool = new Axe();
-        tool.use(controller, new Position(0,0));
-        assertEquals(chunk.getEntityAt(new Position(0,0)), grass);
+        tool.accept(new Target(controller, new Position(0,0)));
+        assertEquals(chunkSpy.getEntities().get(0), grass);
     }
 }
