37dd2afc3c05eaaa3210f41eec0d6eddbb753fa4
Joao Sousa <joaosousa.pt@gmail.com>
2020-05-26 01:45:14 +0100

    Improved Look of Controls "Submenu"

--- src/main/java/com/g64/view/ControlsView.java
+++ src/main/java/com/g64/view/ControlsView.java
@@ -5,38 +5,32 @@ import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.screen.Screen;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class ControlsView {
 
     private TextGraphics graphics;
-    private ArrayList<String> controls = new ArrayList<String>(
-            Arrays.asList(
-                    "W, A, S, D To Move The Character",
-                    "Arrow Keys To Interact With The Surroundings \nUsing The Selected Item",
-                    "0 To 9 To Select An Item From The Inventory",
-                    "[Press Any Key To Go Back To The Main Menu]"
-            )
-    );
 
     public ControlsView(Screen screen) { graphics = screen.newTextGraphics(); }
 
     public void draw() {
 
-        int row = 1;
+        int row = 3;
 
         graphics.setBackgroundColor(TextColor.ANSI.BLACK);
         graphics.setForegroundColor(TextColor.ANSI.WHITE);
 
         graphics.enableModifiers(SGR.BOLD);
 
-        graphics.putString((40 - 8)/2, row += 3, "Controls");
+        graphics.putString((40 - 8)/2, row, "Controls");
 
         graphics.disableModifiers(SGR.BOLD);
 
-        for (String s : controls) {
-            graphics.putString((40 - s.length())/2, row += 2, s);
-        }
+        graphics.putString(4, row += 3, "W, A, S, D To Move The Character");
+        graphics.putString(4, row += 2, "Arrow Keys To Interact With");
+        graphics.putString(6, row += 1, "The Surroundings Using");
+        graphics.putString(6, row += 1, "The Selected Item");
+        graphics.putString(4, row += 2, "0 To 9 To Select An Item");
+        graphics.putString(6, row += 1, "From The Inventory");
+        graphics.putString(4, row += 2, "[Press Any Key To Go Back");
+        graphics.putString(6, row += 1, "To The Main Menu]");
     }
 }
