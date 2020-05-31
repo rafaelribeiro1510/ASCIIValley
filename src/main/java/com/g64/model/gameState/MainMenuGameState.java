5d550bfe544c296aff0491c59991713936c1bdb2
Joao Sousa <joaosousa.pt@gmail.com>
2020-05-31 16:37:34 +0100

    Optimized Imports, Removed Commented Unused Code

--- src/main/java/com/g64/model/gameState/MainMenuGameState.java
+++ src/main/java/com/g64/model/gameState/MainMenuGameState.java
@@ -1,23 +1,15 @@
 package com.g64.model.gameState;
 
 import com.g64.controller.GameController;
-import com.g64.controller.action.*;
 import com.g64.model.menuCommands.ControlsCommand;
 import com.g64.model.menuCommands.MenuOption;
 import com.g64.model.menuCommands.PlayCommand;
 import com.g64.model.menuCommands.QuitCommand;
-import com.googlecode.lanterna.input.KeyStroke;
-import com.googlecode.lanterna.input.KeyType;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainMenuGameState extends MenuGameState {
-    /*
-    private int selectedOption;
-    private GameController gameController;
-    private ArrayList<MenuOption> menuOptions;
-    */
 
     public MainMenuGameState(GameController gameController) {
         this.selectedOption = 0;
@@ -36,24 +28,5 @@ public class MainMenuGameState extends MenuGameState {
         // draw menu
         gameController.getMenuView().draw(this);
     }
-    /*
-    @Override
-    public ActionEvent processKey(KeyStroke key) {
-
-        if (key == null)                            return new NullAction();
-        if (key.getKeyType() == KeyType.ArrowUp)    return new MenuUp(this);
-        if (key.getKeyType() == KeyType.ArrowDown)  return new MenuDown(this);
-        if (key.getKeyType() == KeyType.Enter)      return new EnterPressed(this);
-
-        return new NullAction();
-    }
-    */
-    /*
-    public ArrayList<MenuOption> getMenuOptions() { return menuOptions; }
-
-    public int getSelectedOption() { return selectedOption; }
-
-    public void setSelectedOption(int selectedOption) { this.selectedOption = selectedOption; }
-    */
 
 }
--- src/main/java/com/g64/model/gameState/PauseMenuState.java
+++ src/main/java/com/g64/model/gameState/PauseMenuState.java
@@ -1,21 +1,15 @@
 package com.g64.model.gameState;
 
 import com.g64.controller.GameController;
-import com.g64.controller.action.*;
-import com.g64.model.menuCommands.*;
-import com.googlecode.lanterna.input.KeyStroke;
-import com.googlecode.lanterna.input.KeyType;
+import com.g64.model.menuCommands.MenuOption;
+import com.g64.model.menuCommands.PlayCommand;
+import com.g64.model.menuCommands.QuitCommand;
+import com.g64.model.menuCommands.SaveMapCommand;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class PauseMenuState extends MenuGameState {
-
-    /*
-    private int selectedOption;
-    private GameController gameController;
-    private ArrayList<MenuOption> pauseOptions;
-    */
 
     public PauseMenuState(GameController gameController) {
         this.selectedOption = 0;
@@ -34,25 +28,5 @@ public class PauseMenuState extends MenuGameState {
         // draw pause menu
         gameController.getPauseMenuView().draw(this);
     }
-    /*
-    @Override
-    public ActionEvent processKey(KeyStroke key) {
 
-        if (key == null)                            return new NullAction();
-        if (key.getKeyType() == KeyType.ArrowUp)    return new MenuUp(this);
-        if (key.getKeyType() == KeyType.ArrowDown)  return new MenuDown(this);
-        if (key.getKeyType() == KeyType.Enter)      return new EnterPressed(this);
-
-        return new NullAction();
-    }
-    */
-    /*
-    public ArrayList<MenuOption> getMenuOptions() { return menuOptions; }
-    */
-    /*
-    public int getSelectedOption() { return selectedOption; }
-    */
-    /*
-    public void setSelectedOption(int selectedOption) { this.selectedOption = selectedOption; }
-    */
 }
