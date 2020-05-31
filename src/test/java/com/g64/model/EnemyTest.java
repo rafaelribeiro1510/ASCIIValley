a8afd75365a0a35da15b7f33a068e82783b941b2
up201806330 <45789887+up201806330@users.noreply.github.com>
2020-05-31 23:27:53 +0100

    asdsa

--- src/test/java/com/g64/model/EnemyTest.java
+++ src/test/java/com/g64/model/EnemyTest.java
@@ -2,18 +2,16 @@ package com.g64.model;
 
 import com.g64.controller.GameController;
 import com.g64.controller.commands.*;
-import com.g64.model.entities.EntityModel;
 import com.g64.model.entities.Player;
 import com.g64.model.entities.enemy.Enemy;
 import com.g64.model.entities.enemy.EnemyFactory;
 import com.g64.model.entities.enemy.Ghost;
 import com.g64.model.entities.enemy.Mummy;
-import com.g64.model.entities.enemy.humours.GhostAggroed;
-import com.g64.model.entities.plant.TallGrassEntity;
 import com.g64.model.terrain.GrassTerrain;
 import com.g64.model.terrain.MapTerrain;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.util.ArrayList;
 import java.util.Arrays;
